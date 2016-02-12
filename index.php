81ffafa7e987feb92254151e4b68b22245684b65
Alex Mattingley <alexmattingely@gmail.com>
2016-02-12 12:41:28 -0800

    remove blank spaces

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php 
-echo "server working!";
+<p><?php echo "server working!"; ?></p>
+
+<?php
+
+$config_lines = file('fake_config.txt');
+
+print_r($config_lines . "<br>");
+
+$number_of_lines = count($config_lines);
+
+
+for($i = 0; $i < $number_of_lines; $i++){
+	//print_r($config_lines[$i] . "<br>");
+	if($config_lines[$i] == "\n"){
+		unset($config_lines[$i]);
+	}
+}
+print_r($config_lines);
 
 ?>

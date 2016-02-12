bc8502033d3a8aa5939a35292b917e33d7735160
Alex Mattingley <alexmattingely@gmail.com>
2016-02-12 13:27:16 -0800

    correctly handling comments

--- index.php
+++ index.php
@@ -4,16 +4,31 @@
 
 $config_lines = file('fake_config.txt');
 
-$number_of_lines = count($config_lines);
+$config_file_array = array();
 
+for($i = 0; $i < count($config_lines); $i++){
+	//Remove newlines
+	// if($config_lines[$i] == "\n"){
+	// 	array_splice($config_lines, $i, 1);
+	// }
 
-//Remove an newlines
-for($i = 0; $i < $number_of_lines; $i++){
-	if($config_lines[$i] == "\n"){
-		array_splice($config_lines, $i, 1);
+	if($i == 0){
+		$comment_count = 1;
+	}
+
+	for($j = 0; $j < strlen($config_lines[$i]); $j++){
+		if($j == 0 && $config_lines[$i][0] == "#"){
+			$config_file_array["comment $comment_count"] = $config_lines[$i];
+			$comment_count++;
+		}
+
 	}
 }
-//config_lines should be free of \n here.
+
+
+print_r($config_lines);
+echo "<br>";
+print_r($config_file_array);
 
 
 
